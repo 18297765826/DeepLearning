477b81985212e6a79fee0b207ccfbeba0a08a68e
18297765826 <57345019+18297765826@users.noreply.github.com>
2023-10-31 22:19:38 +0800

    add for AlexNet

---
+++ ModelZoo/AlexNet.py
@@ -0,0 +1,38 @@
+import torchsummary
+import torch
+import torch.nn as nn
+
+#use nn.Sequential() function to build Net
+class AlexNet(nn.Module):
+    def __init__(self,num_classes=10):
+        super(AlexNet,self).__init__()
+        self.feature=nn.Sequential(
+            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # output[48, 27, 27]
+            nn.Conv2d(48, 128, kernel_size=5, padding=2),  # output[128, 27, 27]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # output[128, 13, 13]
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),  # output[192, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),  # output[192, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),  # output[128, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # output[128, 6, 6]
+        )
+        self.classification=nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(128 * 6 * 6, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, num_classes) )
+    def forward(self, x):
+            x = self.feature(x)
+            x = torch.flatten(x, start_dim=1)
+            x = self.classification(x)
+            return x
+alexnet=AlexNet().cuda()
+torchsummary.summary(alexnet,(3,224,224),device="cuda")
